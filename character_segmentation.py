446dad23ce0d761cd457d9468e6aea97e61dc397
Sarthak Vajpayee <itssarthakvajpayee@gmail.com>
2019-09-06 10:16:46 +0530

    Update character_segmentation.py

--- character_segmentation.py
+++ character_segmentation.py
@@ -2,28 +2,25 @@
 def segment_characters(image) :
 
     # Preprocess cropped license plate image
-    img_lp = cv2.resize(image, (333, 75))
-    img_gray_lp = cv2.cvtColor(img_lp, cv2.COLOR_BGR2GRAY)
-    _, img_binary_lp = cv2.threshold(img_gray_lp, 200, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
-    img_binary_lp = cv2.erode(img_binary_lp, (3,3))
-    img_binary_lp = cv2.dilate(img_binary_lp, (3,3))
+    img = cv2.resize(image, (333, 75))
+    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    _, img_binary = cv2.threshold(img_gray, 200, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+    img_erode = cv2.erode(img_binary, (3,3))
+    img_dilate = cv2.dilate(img_erode, (3,3))
 
-    LP_WIDTH = img_binary_lp.shape[0]
-    LP_HEIGHT = img_binary_lp.shape[1]
+    LP_WIDTH = img_dilate.shape[0]
+    LP_HEIGHT = img_dilate.shape[1]
 
     # Make borders white
-    img_binary_lp[0:3,:] = 255
-    img_binary_lp[:,0:3] = 255
-    img_binary_lp[72:75,:] = 255
-    img_binary_lp[:,147:150] = 255
+    img_dilate[0:3,:] = 255
+    img_dilate[:,0:3] = 255
+    img_dilate[72:75,:] = 255
+    img_dilate[:,147:150] = 255
 
     # Estimations of character contours sizes of cropped license plates
-    dimensions = [LP_WIDTH/6,
-                       LP_WIDTH/2,
-                       LP_HEIGHT/10,
-                       2*LP_HEIGHT/3]
+    dimensions = [LP_WIDTH/6, LP_WIDTH/2, LP_HEIGHT/10, 2*LP_HEIGHT/3]
 
     # Get contours within cropped license plate
-    char_list = find_contours(dimensions, img_binary_lp)
+    char_list = find_contours(dimensions, img_dilate)
 
     return char_list
